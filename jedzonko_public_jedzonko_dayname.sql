5d001155abecb85267f00ccce6bba559d28c4761
Joanna Gradowska <joanna.gradowska@icloud.com>
2019-11-12 14:23:41 +0100

    zad 5.1 SQL

---
+++ jedzonko_public_jedzonko_dayname.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (1, 'Poniedziałek', 1);
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (2, 'Wtorek', 2);
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (3, 'Środa', 3);
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (4, 'Czwartek', 4);
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (5, 'Piątek', 5);
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (6, 'Sobota', 6);
+INSERT INTO public.jedzonko_dayname (id, day_name, "order") VALUES (7, 'Niedziela', 7);
