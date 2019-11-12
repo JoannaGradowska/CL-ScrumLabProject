267d1f94b2b4f506e7f20b5fa9ff0934609e3e94
Joanna Gradowska <joanna.gradowska@icloud.com>
2019-11-12 11:07:52 +0100

    zad 1.2 do testow

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+    ingredients = models.TextField()
+    description = models.TextField()
+    created = models.DateTimeField(datetime.now())
+    updated = models.DateTimeField(datetime.now())
+    preparation_time = models.SmallIntegerField()
+    votes = models.SmallIntegerField(default=0)
