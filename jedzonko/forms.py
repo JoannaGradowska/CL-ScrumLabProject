53f6f37b002ed3a7d3ff50a2ee06bf0b55c41c57
dodocanfly <info@dodocanfly.pl>
2019-11-12 16:19:35 +0100

    Update forms.py

--- jedzonko/forms.py
+++ jedzonko/forms.py
@@ -1,9 +1,8 @@
 from django.forms import ModelForm
-
 from jedzonko.models import Recipe
 
 
-class AddRecipeView(ModelForm):
+class AddRecipeForm(ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'ingredients', 'description', 'preparation_time']
