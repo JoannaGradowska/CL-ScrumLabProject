eebbaabbeab7dfb037ff59ae2c059c94d14d5625
dodocanfly <info@dodocanfly.pl>
2019-11-13 11:51:54 +0100

    3.1 Recipes list

--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('main/', views.Dashboard.as_view()),
     path('recipe/<int:id>/', views.RecipeDetails.as_view()),
     path('recipe/list/', views.RecipeList.as_view()),
+    path('recipe/list/<int:page>/', views.RecipeList.as_view()),
     path('recipe/add/', views.RecipeAdd.as_view()),
     path('recipe/modify/<int:id>/', views.RecipeModify.as_view()),
     path('plan/<int:id>/', views.PlanDetails.as_view()),
