7afe96109cadb73f95b676342d6e5f439ee6f702
dodocanfly <info@dodocanfly.pl>
2019-11-13 23:06:20 +0100

    6.1 add plan form

--- jedzonko/forms.py
+++ jedzonko/forms.py
@@ -1,8 +1,18 @@
 from django.forms import ModelForm
-from jedzonko.models import Recipe
+from jedzonko.models import Recipe, Plan
 
 
 class AddRecipeForm(ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'ingredients', 'description', 'preparation_time']
+
+
+class AddPlanForm(ModelForm):
+    class Meta:
+        model = Plan
+        fields = ['name', 'description']
+        labels = {
+            'name': 'Nazwa planu',
+            'description': 'Opis planu',
+        }
